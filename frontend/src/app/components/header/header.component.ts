bc807f21cf1101538d67b82219d28d59b9a073b0
Mxmac <pro.maximeandre@gmail.com>
2025-01-29 20:28:23 +0100

    Component standalone

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-header',
   imports: [],
   templateUrl: './header.component.html',
+  standalone: true,
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
