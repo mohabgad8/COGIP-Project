79271f03edb441b975c9ddd422558897590b6fdd
GabrielHugo <hugogabriel141201@gmail.com>
2025-01-30 16:28:22 +0100

    database update

---
+++ backend/config/database.py
@@ -0,0 +1,40 @@
+import mysql.connector
+import os
+from dotenv import load_dotenv
+from tabulate import tabulate
+load_dotenv()
+
+try:
+    mydb = mysql.connector.connect(
+        database=os.getenv("DB_NAME"),
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        port= int(os.getenv("DB_PORT")),
+        charset = 'utf8mb4',
+        collation = 'utf8mb4_general_ci'
+    )
+
+    cursor = mydb.cursor()
+    cursor.execute("SELECT * FROM companies")
+    result = cursor.fetchall()
+
+    columns = [desc[0] for desc in cursor.description]
+
+    if result:
+        print("result", tabulate(result, headers=columns, tablefmt = "fancy_grid" ))
+        print("Database connection established")
+        print("Result: ", result)
+
+except mysql.connector.Error as err:
+    print("Failed to connect to MySQL database")
+    print(f"Error: {err}")
+    print("Error info: ", err.args)
+
+finally:
+    if 'mydb' in locals():
+        if mydb.is_connected():
+            mydb.close()
+            print("Database closed")
+        else:
+            print("Connection was not established")
