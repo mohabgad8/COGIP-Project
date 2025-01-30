65831e1b1da8ecaa5976d90466836af7e10b801d
GabrielHugo <hugogabriel141201@gmail.com>
2025-01-30 16:29:35 +0100

    import update, invoiceColumn, contactColumn, companies added

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,65 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from './components/header/header.component';
+import {ListComponent} from './components/list/list.component';
 
 
 @Component({
   selector: 'app-root',
   standalone:true,
-  imports:[HeaderComponent],
+  imports: [HeaderComponent, ListComponent],
   templateUrl:'app.component.html',
   styleUrl: 'app.component.css'
 })
 export class AppComponent {
+
+  invoiceColumn = [
+    {
+      key: "invoice number", label: "Invoice number"
+    },
+    {
+      key: "dates due", label: "Dates due"
+    },
+    {
+      key: "company", label: "Company"
+    },
+    {
+      key: "create at", label: "Created at"
+    }]
+
+  contactsColumn = [
+    {
+      key: "name", label: "Name"
+    },
+    {
+      key: "phone", label: "Phone"
+    },
+    {
+      key: "mail", label: "Mail"
+    },
+    {
+      key: "company", label: "Company"
+    },
+    {
+      key: "create at", label: "Create at"
+    }
+  ]
+
+  companiesColumn = [
+    {
+      key: "name", label: "Name"
+    },
+    {
+      key: "tva", label: "TVA"
+    },
+    {
+      key: "country", label: "Country"
+    },
+    {
+      key: "type", label: "Type"
+    },
+    {
+      key: "create at", label: "Create at"
+    }
+  ]
 
 }
