222e36a2be675ce14a0198eb2a845416b2ecb85b
Mxmac <pro.maximeandre@gmail.com>
2025-01-31 13:39:07 +0100

    import du composant et initialisation dans le app component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from './components/header/header.component';
+import {BannerComponent} from './components/banner/banner.component';
 
 
 @Component({
   selector: 'app-root',
   standalone:true,
-  imports:[HeaderComponent],
+  imports:[HeaderComponent, BannerComponent],
   templateUrl:'app.component.html',
   styleUrl: 'app.component.css'
 })
