07efef3b7d4eca8456d81a54726829b9c6a5339b
Mxmac <pro.maximeandre@gmail.com>
2025-01-31 16:33:39 +0100

    import du footer dans la page principale

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from './components/header/header.component';
 import {BannerComponent} from './components/banner/banner.component';
+import {FooterComponent} from './components/footer/footer.component';
 
 
 @Component({
   selector: 'app-root',
   standalone:true,
-  imports:[HeaderComponent, BannerComponent],
+  imports:[HeaderComponent, BannerComponent, FooterComponent],
   templateUrl:'app.component.html',
   styleUrl: 'app.component.css'
 })
