ae8c5dc014baf83ccd1d25fa340cb199a0a01024
GabrielHugo <hugogabriel141201@gmail.com>
2025-02-04 14:54:23 +0100

    component created

---
+++ frontend/src/app/components/contact-people/contact-people.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-people',
+  imports: [],
+  templateUrl: './contact-people.component.html',
+  styleUrl: './contact-people.component.css'
+})
+export class ContactPeopleComponent {
+
+}
