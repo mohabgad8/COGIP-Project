69ae0911db299736a1c1a22f9eb22ec2ee04b90e
mohabgad8 <77176417+houba1337@users.noreply.github.com>
2025-02-04 14:37:29 +0100

    Added minor changes

--- backend/routes/companies_routes.py
+++ backend/routes/companies_routes.py
@@ -1,19 +1,25 @@
 from fastapi import APIRouter, HTTPException
 from config.database import get_connection
+from pydantic import BaseModel, EmailStr
 
 router = APIRouter()
+connection = get_connection()
+cursor = connection.cursor(dictionary = True)
 
+class CompanySearch(BaseModel):
+    name: str
 
-@router.get("/")
-
-async def get_companies():
+# get all companies' infos
+@router.get("/get_company")
+async def get_company(company: CompanySearch):
     try:
-        conn = get_connection()
-        cursor = conn.cursor(dictionary=True)
-        cursor.execute("SELECT * FROM companies")
+        query = """
+            SELECT * FROM companies
+            INNER JOIN types ON companies.type_id = types.id
+        """
+        cursor.execute(query)
         companies = cursor.fetchall()
-        cursor.close()
-        conn.close()
-        return companies
+
+        return {"companies" : companies}
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        raise HTTPException(status_code=404, detail="Data not found")
