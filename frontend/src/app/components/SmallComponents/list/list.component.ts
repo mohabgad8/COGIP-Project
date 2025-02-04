89a3590fd3d64b08b29292b3ceb7d24c3a2d274d
GabrielHugo <hugogabriel141201@gmail.com>
2025-02-04 17:10:30 +0100

    showSearchBar added

--- frontend/src/app/components/SmallComponents/list/list.component.ts
+++ frontend/src/app/components/SmallComponents/list/list.component.ts
@@ -1,11 +1,12 @@
 import {Component, input, Input} from '@angular/core';
-import {NgForOf} from '@angular/common';
+import {NgForOf, NgIf} from '@angular/common';
 
 
 @Component({
   selector: 'app-list',
   imports: [
     NgForOf,
+    NgIf,
   ],
   templateUrl: './list.component.html',
   standalone: true,
@@ -15,5 +16,7 @@ export class ListComponent {
   @Input() title: string | undefined;
   @Input() column: {key: string, label: string}[] = [];
   @Input() data: any[] = [];
+
+  @Input() showSearchBar: boolean = false;
 
 }
