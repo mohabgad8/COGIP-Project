8b85618a115b36ac062f0a6a72c2b7267feb341f
Mxmac <pro.maximeandre@gmail.com>
2025-02-04 14:29:00 +0100

    préparation de la page dashboard

---
+++ frontend/src/app/components/dashboardpage/dashboardpage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboardpage',
+  imports: [],
+  templateUrl: './dashboardpage.component.html',
+  styleUrl: './dashboardpage.component.css'
+})
+export class DashboardpageComponent {
+
+}
