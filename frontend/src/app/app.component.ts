28d3800162a062f3dbaed7721ed71a011be528d1
GabrielHugo <hugogabriel141201@gmail.com>
2025-02-05 10:50:38 +0100

    component pagination added

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {HeaderComponent} from './components/SmallComponents/header/header.component';
 import {FooterComponent} from './components/SmallComponents/footer/footer.component';
+import {PaginationComponent} from './components/SmallComponents/pagination/pagination.component';
 
 
 
 @Component({
   selector: 'app-root',
   standalone:true,
-  imports: [HeaderComponent, FooterComponent, RouterOutlet],
+  imports: [HeaderComponent, FooterComponent, RouterOutlet, PaginationComponent],
   templateUrl:'app.component.html',
   styleUrl: 'app.component.css'
 })
