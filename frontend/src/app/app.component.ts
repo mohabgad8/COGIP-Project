72ac748678155d038c2d2b521fd55c50656feef3
Mxmac <pro.maximeandre@gmail.com>
2025-02-05 09:12:29 +0100

    refacto pour n'utiliser que router outlet

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from '@angular/router';
-import {HeaderComponent} from './components/SmallComponents/header/header.component';
-import {FooterComponent} from './components/SmallComponents/footer/footer.component';
 
 
 
 @Component({
   selector: 'app-root',
   standalone:true,
-  imports: [HeaderComponent, FooterComponent, RouterOutlet],
+  imports: [RouterOutlet],
   templateUrl:'app.component.html',
   styleUrl: 'app.component.css'
 })
