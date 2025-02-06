97e3a14156ced58a2bc35e2191f57ab18b40c74f
Mxmac <pro.maximeandre@gmail.com>
2025-02-06 08:05:32 +0100

    save commit

--- frontend/src/app/components/PagesComponents/dashboardpage/dashboardpage.component.ts
+++ frontend/src/app/components/PagesComponents/dashboardpage/dashboardpage.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import {SidebarComponent} from '../../SmallComponents/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-dashboardpage',
-  imports: [],
+  imports: [
+    SidebarComponent
+  ],
+  standalone:true,
   templateUrl: './dashboardpage.component.html',
   styleUrl: './dashboardpage.component.css'
 })
 export class DashboardpageComponent {
-
 }
