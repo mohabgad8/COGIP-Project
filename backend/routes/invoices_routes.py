f73627fae7e29d0cf00ac075d0eb150daeae1d44
Vespid <farid.elmam@gmail.com>
2025-02-06 14:04:37 +0100

    date dues add on db

--- backend/routes/invoices_routes.py
+++ backend/routes/invoices_routes.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, HTTPException
 from config.database import get_connection
 from pydantic import BaseModel, Field
-from datetime import datetime
+from datetime import datetime, date
 
 
 router = APIRouter()
@@ -11,7 +11,7 @@ class InvoicesVerify(BaseModel):
     id_company : int
 
 class SearchInvoices(BaseModel):
-    name: str = Field(min_length=2, max_length=50)
+    company_name: str = Field(min_length=2, max_length=50)
 
 class DeleteInvoices(BaseModel):
     ref : str = Field(min_length=2, max_length=50)
@@ -55,7 +55,7 @@ async def get_invoices():
         conn = get_connection()
         cursor = conn.cursor(dictionary=True)
 
-        cursor.execute("SELECT invoices.ref, invoices.created_at, companies.name AS company_name FROM invoices LEFT JOIN companies ON invoices.id_company = companies.id ORDER BY created_at DESC")
+        cursor.execute("SELECT invoices.ref, invoices.date_due, invoices.created_at, companies.name FROM invoices LEFT JOIN companies ON invoices.id_company = companies.id ORDER BY created_at DESC")
 
         get_invoice = cursor.fetchall()
 
@@ -73,7 +73,7 @@ async def search_invoices(search: SearchInvoices):
         conn = get_connection()
         cursor = conn.cursor(dictionary=True)
 
-        query = "SELECT invoices.ref, companies.name, invoices.created_at FROM invoices LEFT JOIN companies ON invoices.id_company = companies.id WHERE companies.name = %s"
+        query = "SELECT invoices.ref, invoices.date_due, companies.name, invoices.created_at FROM invoices LEFT JOIN companies ON invoices.id_company = companies.id WHERE companies.name AS company_name = %s"
         values = (search.name, )
 
         cursor.execute(query, values)
@@ -93,7 +93,7 @@ async def get_last_invoices():
         conn = get_connection()
         cursor = conn.cursor(dictionary=True)
 
-        cursor.execute("SELECT invoices.ref, invoices.created_at, companies.name FROM invoices LEFT JOIN companies ON invoices.id_company = companies.id ORDER BY created_at DESC LIMIT 5")
+        cursor.execute("SELECT invoices.ref, invoices.date_due, invoices.created_at, companies.name FROM invoices LEFT JOIN companies ON invoices.id_company = companies.id ORDER BY created_at DESC LIMIT 5")
 
         get_last_invoice = cursor.fetchall()
 
@@ -111,7 +111,7 @@ async def get_last_invoices_company(company_name : str):
         conn = get_connection()
         cursor = conn.cursor(dictionary=True)
 
-        query = "SELECT invoices.ref, invoices.created_at, companies.name AS company_name FROM invoices LEFT JOIN companies ON invoices.id_company = companies.id WHERE companies.name = %s ORDER BY created_at DESC LIMIT 5"
+        query = "SELECT invoices.ref, invoices.date_due, invoices.created_at, companies.name AS company_name FROM invoices LEFT JOIN companies ON invoices.id_company = companies.id WHERE companies.name = %s ORDER BY created_at DESC LIMIT 5"
         values = (company_name,)
 
         cursor.execute(query, values)
@@ -132,8 +132,8 @@ async def create_invoices(invoices: InvoicesVerify ):
         conn = get_connection()
         cursor = conn.cursor(dictionary=True)
 
-        query = "INSERT INTO invoices (ref, id_company) VALUES (%s, %s)"
-        values = (invoices.ref, invoices.id_company)
+        query = "INSERT INTO invoices (date_due, id_company) VALUES (%s, %s)"
+        values = (invoices.date_due, invoices.id_company)
 
         cursor.execute(query, values)
         conn.commit()
@@ -151,23 +151,23 @@ async def create_invoices(invoices: InvoicesVerify ):
         conn.rollback()
         raise HTTPException(status_code=500, detail=str(e))
 
-@router.put("/update_invoice/{invoice_id}")
-async def update_invoices(invoice_id: int, invoices: InvoicesVerify ):
+@router.put("/update_invoice/{date_due}")
+async def update_invoices(date_due: date, invoices: InvoicesVerify ):
     try:
         conn = get_connection()
         cursor = conn.cursor(dictionary=True)
 
-        cursor.execute("SELECT * FROM invoices WHERE id = %s", (invoice_id,))
+        cursor.execute("SELECT * FROM invoices WHERE date_due = %s", (date_due,))
         if not cursor.fetchone():
             raise HTTPException(status_code=404, detail="Facture non trouvé")
 
-        query = "UPDATE invoices SET ref = %s WHERE id = %s"
-        values = (invoices.ref, invoice_id)
+        query = "UPDATE invoices SET ref = %s, id_company = %s WHERE date_due = %s"
+        values = (invoices.ref, invoices.id_company, date_due)
 
         cursor.execute(query, values)
         conn.commit()
 
-        cursor.execute ("SELECT * FROM invoices WHERE id = %s", (invoice_id,))
+        cursor.execute ("SELECT * FROM invoices WHERE date_due = %s", (date_due,))
         update_invoice = cursor.fetchone()
 
         cursor.close()
@@ -179,13 +179,13 @@ async def update_invoices(invoice_id: int, invoices: InvoicesVerify ):
         conn.rollback()
         raise HTTPException(status_code=500, detail=str(e))
 
-@router.delete("/delete_invoice/{invoice_id}")
-async def delete_invoice(invoice_id: int, invoices: DeleteInvoices):
+@router.delete("/delete_invoice/{date_due}")
+async def delete_invoice(date_due: date, invoices: DeleteInvoices):
     try:
         conn = get_connection()
         cursor = conn.cursor(dictionary=True)
 
-        cursor.execute("SELECT * FROM invoices WHERE id = %s", (invoice_id,))
+        cursor.execute("SELECT * FROM invoices WHERE date_due = %s", (date_due,))
         if not cursor.fetchone():
             raise HTTPException(status_code=404, detail="Contact non trouvé")
 
