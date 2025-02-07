b9d31d9e90015515843cd50933bbe4776524024a
Mxmac <pro.maximeandre@gmail.com>
2025-02-07 13:55:36 +0100

    composant detail fonctionnel, dynamique, en fonction de companies ou contacts

--- frontend/src/app/components/PagesComponents/company-details-page/company-details.component.ts
+++ frontend/src/app/components/PagesComponents/company-details-page/company-details.component.ts
@@ -1,13 +1,13 @@
 import {Component, OnInit} from '@angular/core';
-import {CompanyInfoComponent} from '../../SmallComponents/company-info/company-info.component';
 import {ListComponent} from "../../SmallComponents/list/list.component";
 import {ApiService} from "../../../service/api.service";
 import {ActivatedRoute} from '@angular/router';
+import {DetailsComponent} from '../../SmallComponents/details/details.component';
 
 @Component({
   selector: 'app-company-details-page',
     imports: [
-        CompanyInfoComponent,
+        DetailsComponent,
         ListComponent
     ],
   standalone:true,
@@ -18,27 +18,41 @@ export class CompanyDetailsComponent implements OnInit{
 
    companyName!: string
    invoices: any[] = []
+   companyData: any = null;
    columns= [
     { key: 'ref', label: 'Invoice Number' },
-    { key: 'dates_due', label: 'Dates' },
-    { key: 'name', label: 'Company'},
+    { key: 'date_due', label: 'Dates' },
+    { key: 'company_name', label: 'Company'},
     { key: 'created_at', label: 'Created At'}
   ]
 
 
-  constructor(private apiService: ApiService, private route: ActivatedRoute) {}
- ngOnInit():void{
+ constructor(private route: ActivatedRoute, private apiService: ApiService) {}
 
-  this.route.paramMap.subscribe(params => {
-    this.companyName = params.get('company_name') || '';
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      const companyName = params.get('companyName');
+      if (companyName) {
+        this.fetchCompanyData(companyName);
+        this.fetchInvoices(companyName);
+      }
+    });
+  }
 
-    if(this.companyName) {
-    this.apiService.fetchData(`get_last_invoices_company/${this.companyName}`).subscribe((data:any[]) => {
-      this.invoices = data
-      });
-    }
-  });
-}
+  fetchCompanyData(companyName: string) {
+    this.apiService.fetchData(`get_company/${companyName}`).subscribe({
+      next: (data) => this.companyData = data[0],
+      error: (error) => console.error('Erreur lors de la récupération des données entreprise', error),
+      complete: () => console.log('Données entreprise chargées')
+    });
+  }
 
+  fetchInvoices(companyName: string) {
+    this.apiService.fetchData(`get_last_invoices_company/${companyName}`).subscribe({
+      next: (data) => this.invoices = data,
+      error: (error) => console.error('Erreur lors de la récupération des factures', error),
+      complete: () => console.log('Factures chargées')
+    });
+  }
 }
 
